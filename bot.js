a53ffa35a7486443247d25bdf53f0dec7f7d7f66
ExtremBot <44088452+ExtremBot@users.noreply.github.com>
2018-10-16 16:41:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -117,11 +117,23 @@ var reactionRoles = [];
 var definedReactionRole = null;
 
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
 
 
-
-
-
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
 
 
 client.on('message', message => {
